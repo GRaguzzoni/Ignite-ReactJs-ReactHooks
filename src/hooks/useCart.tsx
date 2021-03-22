3c376f4f77c8bc87ea229021df732a3bec151ccb
GRaguzzoni <guilherme.raguzzoni@gmail.com>
2021-03-22 19:26:00 -0300

    all tests passed

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -1,5 +1,7 @@
 import { createContext, ReactNode, useContext, useState } from 'react';
 import { toast } from 'react-toastify';
+import { setTokenSourceMapRange } from 'typescript';
+import { ProductList } from '../pages/Home/styles';
 import { api } from '../services/api';
 import { Product, Stock } from '../types';
 
@@ -23,28 +25,72 @@ const CartContext = createContext<CartContextData>({} as CartContextData);
 
 export function CartProvider({ children }: CartProviderProps): JSX.Element {
   const [cart, setCart] = useState<Product[]>(() => {
-    // const storagedCart = Buscar dados do localStorage
+  const storagedCart = localStorage.getItem('@RocketShoes:cart');
 
-    // if (storagedCart) {
-    //   return JSON.parse(storagedCart);
-    // }
+  if (storagedCart) {
+     return JSON.parse(storagedCart);
+  }
 
     return [];
   });
 
   const addProduct = async (productId: number) => {
     try {
-      // TODO
+      const {data: stock} = await api.get(`/stock/${productId}`);
+      
+      let cartNewAmount = [...cart];
+      const productAlreadyInCart =  cart.find(product => product.id === productId);
+      
+      if (productAlreadyInCart) {
+        const productAvaiableInStock = stock.amount > productAlreadyInCart.amount;
+        console.log(productAlreadyInCart.amount);
+        if(!productAvaiableInStock) {
+          toast.error('Quantidade solicitada fora de estoque');
+          return;
+        }
+        const updateCartAmount = cart.map(product => {
+          return product.id === productId
+          ? {...product, amount: productAlreadyInCart.amount + 1}
+          : product;
+        })
+
+        localStorage.setItem('@RocketShoes:cart',JSON.stringify(updateCartAmount));
+        setCart(updateCartAmount);
+        
+      } else {  
+
+          const productResponse = await api.get(`/products/${productId}`);
+          cartNewAmount = [
+            ...cart, {
+              ...productResponse.data,
+              amount: 1
+            },
+          ];
+
+        localStorage.setItem('@RocketShoes:cart',JSON.stringify(cartNewAmount));
+        setCart(cartNewAmount);          
+      }
+        
+         
     } catch {
-      // TODO
+      toast.error('Erro na adição do produto');
     }
   };
 
   const removeProduct = (productId: number) => {
     try {
-      // TODO
+      const productExists = cart.find(product => product.id === productId);
+        if (!productExists) {
+          toast.error("Erro na remoção do produto");
+          return;
+        }
+
+      const cartFiltered = cart.filter(product => product.id !== productId);
+      localStorage.setItem('@RocketShoes:cart',JSON.stringify(cartFiltered));
+      setCart(cartFiltered);
+
     } catch {
-      // TODO
+     toast.error("Erro na remoção do produto");
     }
   };
 
@@ -53,9 +99,32 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
     amount,
   }: UpdateProductAmount) => {
     try {
-      // TODO
+      if (amount <= 0) return;
+
+      const {data: stock} = await api.get(`/stock/${productId}`);
+      const productExistInCart = cart.find(product => product.id === productId);
+
+      if (!productExistInCart){
+        throw Error();
+      }
+
+      const productInStock = stock.amount >= amount;
+
+      if (!productInStock){
+        toast.error("Quantidade solicitada fora de estoque");
+        return;
+      }
+
+      const updateCartAmount = cart.map(product => {
+        return product.id === productId
+        ? {...product, amount} : product;
+      })
+
+      localStorage.setItem('@RocketShoes:cart', JSON.stringify(updateCartAmount));
+      setCart(updateCartAmount);
+        
     } catch {
-      // TODO
+      toast.error("Erro na alteração de quantidade do produto");
     }
   };
 
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -22,44 +22,49 @@ interface CartItemsAmount {
 }
 
 const Home = (): JSX.Element => {
-  // const [products, setProducts] = useState<ProductFormatted[]>([]);
-  // const { addProduct, cart } = useCart();
+  const [products, setProducts] = useState<ProductFormatted[]>([]);
+  const { addProduct, cart } = useCart();
 
-  // const cartItemsAmount = cart.reduce((sumAmount, product) => {
-  //   // TODO
-  // }, {} as CartItemsAmount)
+  const cartItemsAmount = cart.reduce((sumAmount, product) => {
+    sumAmount[product.id] = product.amount
+    return sumAmount
+  }, {} as CartItemsAmount)
 
   useEffect(() => {
     async function loadProducts() {
-      // TODO
+      api.get('/products')
+      .then(response => setProducts(response.data))
     }
 
     loadProducts();
   }, []);
 
   function handleAddProduct(id: number) {
-    // TODO
+    addProduct(id);
   }
 
   return (
+    
     <ProductList>
-      <li>
-        <img src="https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg" alt="Tênis de Caminhada Leve Confortável" />
-        <strong>Tênis de Caminhada Leve Confortável</strong>
-        <span>R$ 179,90</span>
-        <button
-          type="button"
-          data-testid="add-product-button"
-        // onClick={() => handleAddProduct(product.id)}
-        >
-          <div data-testid="cart-product-quantity">
-            <MdAddShoppingCart size={16} color="#FFF" />
-            {/* {cartItemsAmount[product.id] || 0} */} 2
-          </div>
+      {products.map(product => (
+          <li key={product.id}>
+            <img src={product.image} alt="Img" />
+            <strong>{product.title}</strong>
+            <span>{formatPrice(product.price)}</span>
+            <button
+              type="button"
+              data-testid="add-product-button"
+             onClick={() => handleAddProduct(product.id)}
+            >
+              <div data-testid="cart-product-quantity">
+                <MdAddShoppingCart size={16} color="#FFF" />
+                 {cartItemsAmount[product.id] || 0}  
+              </div>
 
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
+              <span>ADICIONAR AO CARRINHO</span>
+            </button>
+          </li>
+      ))}
     </ProductList>
   );
 };
